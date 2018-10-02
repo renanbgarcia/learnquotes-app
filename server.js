eba0c1c73a1b0c6b4746d4ca7605a88ebb402c52
Renan Garcia <bg_renan@hotmail.com>
2018-10-01 22:08:37 -0300

    Corrige porta do servidor

--- server.js
+++ server.js
@@ -384,6 +384,6 @@ app.route('*').get((req, res) => {
   res.sendFile(__dirname + '/dist/learnquotes/index.html');
 });
 
-app.listen('3000', function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Servidor rodando na porta 3000!!")
 });
