a02b50206e035744ba443cf8c94a71261cf43a8b
Renan Garcia <bg_renan@hotmail.com>
2019-02-20 13:09:02 -0300

    fix word deletion

--- src/app/components/vocab/vocab.component.ts
+++ src/app/components/vocab/vocab.component.ts
@@ -96,8 +96,9 @@ export class VocabComponent implements OnInit {
   openOptions(word) {
     const initialState = { word: word }
     this.modalRef = this.modalService.show(ModalWordOptionComponent, { initialState });
+
     this.modalRef.content.action.take(1).subscribe((value) => {
-      console.log(value); this.userWords.next(value); this.preShowWords(value);
+      console.log(value); this.userWords.next(value); this.getWords(); this.preShowWords(value);
       });
   }
 
