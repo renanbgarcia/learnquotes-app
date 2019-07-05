be9925c1c982f11d1751f6375e44a7f912c108b4
Renan Garcia <bg_renan@hotmail.com>
2019-07-05 20:05:40 -0300

    fix environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  ENDPOINT: 'https://lingo-api.herokuapp.com/',
+  GOOGLE_AUTH_ENDPOINT: 'http://localhost:3000/auth/google'
 };
